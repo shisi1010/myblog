17601b2a422b9587c28c6944851a0ad95c2be342
jinfangxin <865893404@qq.com>
2017-04-05 17:15:59 +0800

    第9章之前的样例，后续应该会加

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by shisi on 2017/4/5.
+ */
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+};
